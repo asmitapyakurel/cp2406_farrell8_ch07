70eb029d1b7cd4d61c01ef5290238bd1dff1a925
Asmita Pyakurel <asmita.pyakurel@my.jcu.edu.au>
2020-08-31 12:51:41 +1000

    Reformat and debug codes

--- DebuggingExercises/DebugSeven2.java
+++ DebuggingExercises/DebugSeven2.java
@@ -2,6 +2,7 @@
 // separated by spaces
 // Program converts them to numbers and sums them
 import java.util.*;
+
 public class DebugSeven2
 {
    public static void main(String[] args)
@@ -17,23 +18,24 @@ public class DebugSeven2
       Scanner in = new Scanner(System.in);
       System.out.print("Enter a series of integers separated by spaces >> ");
       str = in.nextLine();
-      length = length();
+      length = str.length();
       for(x = 0; x <= length; ++x)
       {
-         if(str.charAt(x) == " ")
+         if(str.charAt(x) == ' ')
          {
-             partStr = str.substring(x, lastSpace + 1);     
-             num = Integer.parseInt(partStr);
-             System.out.println("                " + num);
-             sum = num;
-             lastSpace == x;
-          } 
+            partStr = str.substring(x, lastSpace + 1);
+            num = Integer.parseInt(partStr);
+            System.out.println("                " + num);
+            sum += num;
+            lastSpace = x;
+         }
+
       }
       partStr = str.substring(lastSpace + 1, length);
       num = Integer.parseInt(partStr);
       System.out.println("                " + num);
-      sum = num;
+      sum += num;
       System.out.println("         -------------------" +
-         "\nThe sum of the integers is " + sum);
+              "\nThe sum of the integers is " + sum);
    }
 }
--- DebuggingExercises/DebugSeven4.java
+++ DebuggingExercises/DebugSeven4.java
@@ -1,4 +1,6 @@
-// Converts a string to lowercase, and 
+import static java.lang.Character.*;
+
+// Converts a string to lowercase, and
 // displays the string's length
 // as well as a count of letters
 public class DebugSeven4
@@ -7,18 +9,18 @@ public class DebugSeven4
    {
       String aString = "HELP!! I need to get 37 things DONE today!!";
       int numLetters = 0;
-      int stringLength = aString.length;
-      System.out.println("In all lowercase, the sentence is: ");
-      for(int i = 0; i < length; i++)
+      int stringLength = aString.length();
+      System.out.println("In all lowercase, the sentence is: " + aString.toLowerCase());
+      for(int i = 0; i < aString.length(); i++)
       {
-         char ch = Character.toLowerCase(aString.charAt(length));
+         char ch = Character.toLowerCase(aString.charAt(stringLength));
          System.out.print(ch);
-         if(Character.isLetter())
+         if(Character.isLetter(Integer.parseInt(aString)))
             numLetters++;
       }
       System.out.println();
       System.out.println
-         ("The number of CHARACTERS in the string is " + stringLength);
+              ("The number of CHARACTERS in the string is " + stringLength);
       System.out.println("The number of LETTERS is " + stringLength);
    }
 }
